807df3a7ba7c82f3eb73f4a3de31f77a7549e741
Roland Crosby <roland@rolandcrosby.com>
2020-04-01 15:01:59 -0400

    Add problem statement and tricky solution

--- 20200401-singlenumber.py
+++ 20200401-singlenumber.py
@@ -1,3 +1,17 @@
+# 136. Single Number
+# Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+# 
+# Note:
+# Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+# 
+# Example 1:
+# Input: [2,2,1]
+# Output: 1
+# 
+# Example 2:
+# Input: [4,1,2,1,2]
+# Output: 4
+
 from typing import List
 
 class Solution:
@@ -12,13 +26,21 @@ class Solution:
                 possible.add(n)
         return list(possible)[0]
 
+class AnnoyingSolution:
+    def singleNumber(self, nums: List[int]) -> int:
+        """After submitting, I heard that this is the "right" answer that
+         doesn't require you to use extra memory. Thanks, I hate it."""
+        from functools import reduce
+        return reduce(lambda x, y: x ^ y, nums, 0)
+
 if __name__ == "__main__":
     testdata = [
         ([2, 2, 1], 1),
         ([4, 1, 2, 1, 2], 4)
     ]
-    passed = 0
-    for tc in testdata:
-        assert Solution().singleNumber(tc[0]) == tc[1]
-        passed += 1
-    print("%d of %d tests passed" % (passed, len(testdata)))
+    for klass in [Solution, AnnoyingSolution]:
+        passed = 0
+        for tc in testdata:
+            assert klass().singleNumber(tc[0]) == tc[1]
+            passed += 1
+        print("%s: %d of %d tests passed" % (klass.__name__, passed, len(testdata)))
