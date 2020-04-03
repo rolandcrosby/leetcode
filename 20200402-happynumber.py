964efa146ee2914fe8e82d17b5b6591b678f8b19
Roland Crosby <roland@rolandcrosby.com>
2020-04-02 20:36:39 -0400

    Add fancier cycle detection solution

--- 20200402-happynumber.py
+++ 20200402-happynumber.py
@@ -30,6 +30,25 @@ class Solution:
             seen.add(n)
         return True
 
+class FloydSolution:
+    """Fancy solution using Floyd's Tortoise and Hare algorithm (which I just
+    learned about) to detect a cycle without requiring any additional space."""
+    def isHappy(self, n: int) -> bool:
+        def sumSquaresDigits(num: int) -> int:
+            acc = 0
+            while num > 0:
+                digit = num % 10
+                acc += digit * digit
+                num = num // 10
+            return acc
+        slow = fast = n
+        while fast != 1:
+            fast = sumSquaresDigits(sumSquaresDigits(fast))
+            slow = sumSquaresDigits(slow)
+            if fast == slow:
+                return False
+        return True
+
 if __name__ == "__main__":
     testdata = [
         (0, False),
@@ -38,8 +57,9 @@ if __name__ == "__main__":
         (7, True),
         (19, True)
     ]
-    passed = 0
-    for tc in testdata:
-        assert Solution().isHappy(tc[0]) == tc[1]
-        passed += 1
-    print("%d of %d tests passed" % (passed, len(testdata)))
+    for klass in [Solution, FloydSolution]:    
+        passed = 0
+        for tc in testdata:
+            assert klass().isHappy(tc[0]) == tc[1]
+            passed += 1
+        print("%s: %d of %d tests passed" % (klass.__name__, passed, len(testdata)))
