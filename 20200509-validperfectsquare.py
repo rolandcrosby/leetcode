47ff5bf19687f0ed163a843f8c72cb9ba27cf123
Roland Crosby <roland@rolandcrosby.com>
2020-05-10 15:01:22 +0000

    5/9: Valid Perfect Square

---
+++ 20200509-validperfectsquare.py
@@ -0,0 +1,32 @@
+# Valid Perfect Square
+#
+# Given a positive integer num, write a function which returns True if num is a perfect square else False.
+#
+# Note: Do not use any built-in library function such as sqrt.
+#
+# Example 1:
+# Input: 16
+# Output: true
+#
+# Example 2:
+# Input: 14
+# Output: false
+
+import testlib
+
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        n = 1
+        while n * n < num:
+            n += 1
+        return n * n == num
+
+
+if __name__ == "__main__":
+    testdata = [(16, True), (14, False)]
+    testlib.run(
+        lambda t, tc: t.assertEqual(Solution().isPerfectSquare(tc[0]), tc[1], tc),
+        testdata,
+    )
+
