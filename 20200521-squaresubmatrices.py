3d72d3e761f955cfef9a77022dd47bf964b8a6aa
Roland Crosby <roland@rolandcrosby.com>
2020-05-22 03:55:51 +0000

    5/21: Square Submatrices

---
+++ 20200521-squaresubmatrices.py
@@ -0,0 +1,63 @@
+# Count Square Submatrices with All Ones
+# Given a m * n matrix of ones and zeros, return how many square submatrices have all
+# ones.
+#
+# Example 1:
+# Input: matrix =
+# [
+#   [0,1,1,1],
+#   [1,1,1,1],
+#   [0,1,1,1]
+# ]
+# Output: 15
+# Explanation:
+# - There are 10 squares of side 1.
+# - There are 4 squares of side 2.
+# - There is  1 square of side 3.
+# - Total number of squares = 10 + 4 + 1 = 15.
+#
+# Example 2:
+# Input: matrix =
+# [
+#   [1,0,1],
+#   [1,1,0],
+#   [1,1,0]
+# ]
+# Output: 7
+# Explanation:
+# - There are 6 squares of side 1.
+# - There is 1 square of side 2.
+# - Total number of squares = 6 + 1 = 7.
+#
+# Constraints:
+# - 1 <= arr.length <= 300
+# - 1 <= arr[0].length <= 300
+# - 0 <= arr[i][j] <= 1
+
+from typing import List
+import testlib
+
+
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        ans = 0
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == 1 and r > 0 and c > 0:
+                    matrix[r][c] = (
+                        min(matrix[r - 1][c - 1], matrix[r][c - 1], matrix[r - 1][c],)
+                        + 1
+                    )
+                ans += matrix[r][c]
+        return ans
+
+
+if __name__ == "__main__":
+    testdata = [
+        ([[1, 1], [1, 1]], 5),
+        ([[0, 1, 1, 1], [1, 1, 1, 1], [0, 1, 1, 1]], 15),
+        ([[1, 0, 1], [1, 1, 0], [1, 1, 0]], 7),
+    ]
+    testlib.run(
+        lambda t, tc: t.assertEqual(Solution().countSquares(tc[0]), tc[1], tc), testdata
+    )
